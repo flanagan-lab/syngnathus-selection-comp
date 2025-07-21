ffb316d00962c69dfb2a3a30cf97cc042e287855
Sarah P. Flanagan <spflanagan@users.noreply.github.com>
2025-07-22 09:10:07 +1200

    creating a function to partition female opportunity for selection
    based on Coley's code in the floridae analysis Rmd

---
+++ R/partition_fem_I.R
@@ -0,0 +1,61 @@
+# function to partition the opportunity for selection for female pipefish
+partition_fem_I <- function(fem_succ){
+
+    #Calculate the absolute pre-copulatory fitness (Eq. 14 Arnold & Wade 1984)
+    #This is the same as the calculation of I_s
+    fem_succ$w1 <- fem_succ$MatingSuccess/mean(fem_succ$MatingSuccess) #Relative mating success
+    p <- (1)/(nrow(fem_succ) - 1)
+    
+    I_1 <- sum(p*((fem_succ$w1 - 1)^2))
+    
+    #Post-copulatory selection event 1 (Number of eggs transferred per mate) (Eq. 15 Arnold & Wade 1984)
+    fem_succ$W1W2 <- fem_succ$totalEggs/mean(fem_succ$totalEggs) #Relative num. eggs transferred
+    fem_succ$w2 <- fem_succ$W1W2/fem_succ$w1 #Relative num. eggs transferred PER MATE
+    fem_succ$w2[is.na(fem_succ$w2)] <- 0
+    p1 <- fem_succ$w1/(nrow(fem_succ) - 1)
+    
+    I_2 <- sum(p1*((fem_succ$w2 - 1)^2))
+    
+    #Post-copulatory selection event 2 (Proportion eggs developed) (Eq. 16 Arnold & Wade 1984)
+    fem_succ$W1W2W3 <- fem_succ$NumDeveloped/mean(fem_succ$NumDeveloped) #Relative num. eggs developed
+    fem_succ$w3 <- fem_succ$W1W2W3/fem_succ$W1W2 #Relative PROPORTION eggs developed
+    fem_succ$w3[is.na(fem_succ$w3)] <- 0
+    p2 <- (fem_succ$w1*fem_succ$w2)/(nrow(fem_succ)-1)
+    
+    I_3 <- sum(p2*((fem_succ$w3 - 1)^2))
+    
+    I_12 <- var(fem_succ$W1W2)/(mean(fem_succ$W1W2)^2)
+    
+    #Total opportunity for selection
+    I <- var(fem_succ$W1W2W3)/(mean(fem_succ$W1W2W3)^2)
+    
+    ##Calculating the co-variances
+    coi1_2 <- cov(fem_succ$w1,fem_succ$w2) ##COI(1, 2) Eq. A1
+    coi1_2given1 <- ((1/(nrow(fem_succ) - 1))*sum((fem_succ$w1^2)*fem_succ$w2)) - ((1/(nrow(fem_succ) - 1))*sum(fem_succ$w1^2)) ##COI(1, 2|1) Eq. A2
+    
+    coi12_2 <- (1/(nrow(fem_succ) - 1))*sum(fem_succ$w1*(fem_succ$w2^2)) - (1/(nrow(fem_succ) - 1))*sum(fem_succ$w2) ##COI(12, 2) Eq. A3
+    coi12_2given1 <- ((1/(nrow(fem_succ) - 1))*sum((fem_succ$w1^2)*(fem_succ$w2^2))) - ((1/(nrow(fem_succ) - 1))*sum((fem_succ$w1^2)*fem_succ$w2)) ##COI(12, 2|1) Eq. A4
+    
+    diff_12 <- coi12_2given1 - coi12_2 ##Used in Eq. A5 for calculating I_12
+    
+    coi12_3 <- sum(p*((fem_succ$w1*fem_succ$w2) - 1)*(fem_succ$w3 - 1)) ##COI(12, 3) Eq. A6
+    coi12_3given2 <- ((1/(nrow(fem_succ) - 1))*sum((fem_succ$w1^2)*(fem_succ$w2^2)*(fem_succ$w3))) - ((1/(nrow(fem_succ) - 1))*sum((fem_succ$w1^2)*(fem_succ$w2^2))) ##COI(12, 3|2) Eq. A9
+    
+    coi123_3 <- sum(p*((fem_succ$w1*fem_succ$w2*fem_succ$w3) - 1)*(fem_succ$w3 - 1)) ##COI(123, 3) Eq. A10
+    coi123_3given2 <- sum(p2*(((fem_succ$w1*fem_succ$w2*fem_succ$w3) - 1)*(fem_succ$w3 - 1))) ##COI(123, 3|2) Eq. All
+    
+    diff_123 <- coi123_3given2 - coi123_3 ##Used in Eq. A12 for calculating I
+    
+    
+    #Combining all of the selection values (Is) and covariances (COIs) and saving the output
+    selection <- cbind(I_1, I_2, coi1_2, coi1_2given1, coi12_2, coi12_2given1,
+                       diff_12, I_12, I_3, coi12_3, coi12_3given2, coi123_3, coi123_3given2,
+                       diff_123, I)
+    colnames(selection)<-  c("I_1", "I_2", "coi1_2",
+                             "coi1_2given1", "coi12_2", "coi12_2given1",
+                             "diff_12", "I_12", "I_3", "coi12_3",
+                             "coi12_3given2", "coi123_3", "coi123_3given2",
+                             "diff_123", "I")
+    return(selection)
+  
+}
